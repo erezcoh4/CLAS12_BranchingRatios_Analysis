f0e7329867152d55aab52b6116d33268341b2727
erezcoh4 <cohen.erez7@gmail.com>
2024-12-05 11:28:55 +0200

    Debug

--- c12rSkimmer_BranchingRatios.C
+++ c12rSkimmer_BranchingRatios.C
@@ -30,7 +30,7 @@
 #endif
 
 using namespace clas12;
-//BranchingRatio_CLAS12_Auxiliary aux;
+BranchingRatio_CLAS12_Auxiliary aux;
 
 // Results in CSV file d(e,e'p2𝛾X)
 TString csvheader = ( (TString)"status,runnum,evnum,beam_helicity,"
