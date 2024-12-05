f0de64b2e36f0a5c4cd532c75ecfd592d48124de
erezcoh4 <cohen.erez7@gmail.com>
2024-12-05 11:46:06 +0200

    Debug

--- c12rSkimmer_BranchingRatios.C
+++ c12rSkimmer_BranchingRatios.C
@@ -90,7 +90,7 @@ void DEBUG(int v, const char* fmt, ...) {
     va_list arg;
     va_start(arg, fmt);
     std::cout << "Debug(), verbosity: " << verbosity << ", v: " << v << std::endl;
-    if (v > verbosity) {
+    if (verbosity > v) {
         vprintf(fmt, arg);
         std::cout << std::endl;
     }
