a4bcacbb13cdde673cb824d66c22cca4807c5f78
erezcoh4 <cohen.erez7@gmail.com>
2024-12-12 16:17:34 +0200

    Debug

--- c12rSkimmer_BranchingRatios.C
+++ c12rSkimmer_BranchingRatios.C
@@ -640,32 +640,32 @@ void c12rSkimmer_BranchingRatios(int            RunNumber = 6164,
             if (event%PrintProgress==0 && (event > FirstEvent))
                 DEBUG(3,"Start processing %d/%d (run %d, event %d)",event,NeventsMaxToProcess,runnum,evnum);
             
-            if (event > FirstEvent) {
-                
-                runnum = c12.runconfig()->getRun();
-                evnum  = c12.runconfig()->getEvent();
-                
-                // Get Particles By Type
-                electrons   = c12.getByID( 11   );
-                protons     = c12.getByID( 2212 );
-                gammas      = c12.getByID( 22   );
-                GetParticlesByType ();
-                
-                // filter events, extract information, and compute event kinematics
-                if(( 0 < Ne ) && ( Np == 1 ) && ( Ngammas == 2 ))   {
-                    
-                    DEBUG(2,"Extracting information...");
-                    ExtractElectronInformation  ();
-                    ComputeElectronKinematics   ();
-                    ExtractProtonInformation    ();
-                    WriteEventToOutput          ();
-                    DEBUG(2,"Done extracting information...");
-                    
-                } else {
-                    DEBUG(2,"Skipped computation, since N(e)=%d, N(p)=%d, N(gamma)=%d",Ne,Np,Ngammas);
-                }
-                //                Nevents_processed++;
-            } // end if (event > FirstEvent)
+//            if (event > FirstEvent) {
+//                
+//                runnum = c12.runconfig()->getRun();
+//                evnum  = c12.runconfig()->getEvent();
+//                
+//                // Get Particles By Type
+//                electrons   = c12.getByID( 11   );
+//                protons     = c12.getByID( 2212 );
+//                gammas      = c12.getByID( 22   );
+//                GetParticlesByType ();
+//                
+//                // filter events, extract information, and compute event kinematics
+//                if(( 0 < Ne ) && ( Np == 1 ) && ( Ngammas == 2 ))   {
+//                    
+//                    DEBUG(2,"Extracting information...");
+//                    ExtractElectronInformation  ();
+//                    ComputeElectronKinematics   ();
+//                    ExtractProtonInformation    ();
+//                    WriteEventToOutput          ();
+//                    DEBUG(2,"Done extracting information...");
+//                    
+//                } else {
+//                    DEBUG(2,"Skipped computation, since N(e)=%d, N(p)=%d, N(gamma)=%d",Ne,Np,Ngammas);
+//                }
+//                //                Nevents_processed++;
+//            } // end if (event > FirstEvent)
             if (event%PrintProgress==0 && (event > FirstEvent)){
                 DEBUG(1,"Done %d/%d",event,NeventsMaxToProcess);
                 DEBUG(3,"----------------------------------------------------------");
